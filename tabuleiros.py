aee19ab7683bff169e9c856eff2b5876874c60d6
Pedro Henrick <pedrohenrickqa@gmail.com>
2021-09-23 21:03:08 -0300

    implementei uma parte do codigo do professor

--- tabuleiros.py
+++ tabuleiros.py
@@ -8,26 +8,26 @@ def tabuleiro(qtdNav):
         j = randint(0, 9)
         k = randint(0, 9)
 
-        if j>0 and k>0 and tab[j][k] == 'N':
+        if j>0 and k>0 and tab[j-1][k-1] == 'N':
             continue
-        if j>0 and tab[j][k] == 'N':
+        if j>0 and tab[j-1][k] == 'N':
             continue
-        if j>0 and k<9 and tab[j][k] == 'N':
+        if j>0 and k<9 and tab[j-1][k+1] == 'N':
             continue
-        if k>0 and tab[j][k] == 'N':
+        if k>0 and tab[j][k-1] == 'N':
             continue
         if tab[j][k] == 'N':
             continue
-        if k<9 and tab[j][k] == 'N':
+        if k<9 and tab[j][k+1] == 'N':
             continue
-        if j<9 and k>0 and tab[j][k] == 'N':
+        if j<9 and k>0 and tab[j+1][k-1] == 'N':
             continue
-        if j<9 and tab[j][k] == 'N':
+        if j<9 and tab[j+1][k] == 'N':
             continue
-        if j<9 and k<9 and tab[j][k] == 'N':
+        if j<9 and k<9 and tab[j+1][k+1] == 'N':
             continue
         tab[j][k] = 'N'
-        cont += 0
+        cont += 1
     return tab
 
 def tabuleiro2():
